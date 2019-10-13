6ab290119c9d2f157f3842188ce6c99e4fdb65a1
bestialyokel <danil.37910@gmail.com>
2019-10-13 22:40:40 +0300

    lab1 oop edit

--- OOP/лаб1/set.cpp
+++ OOP/лаб1/set.cpp
@@ -13,11 +13,13 @@ struct Node {
 };
 
 struct List {
-    Node *Head;
+    Node *Head = NULL;
+    uint size = 0;
     List() {};
     void push(uint number) {
         if (Head == NULL) {
             Head = new Node(number);
+            size = 1;
             return;
         }
         Node *p = Head;
@@ -29,29 +31,29 @@ struct List {
             Node *q = new Node(number);
             q->Prev = p;
             p->Next = q;
+            size++;
         }
     }           
     void del(uint number) {
         Node *p = Head;
-        if (Head->value == number) {
-            Head = Head->Next;
-            p = NULL;
-            delete p;
-            return;
-        }
-        while (p) {
-            if (p->value == number) {
-                p->Next->Prev = p->Prev;
-                p->Prev->Next = p->Next;
-                delete p;
-                return;
-            }
+        while(p) {
+            if (p->value == number) break;
             p = p->Next;
         }
+        if (p == NULL) return;
+
+        if (p == Head) Head = Head->Next;
+        else if (p->Next == NULL) p->Prev->Next = NULL;
+        else {
+            p->Prev->Next = p->Next;
+            p->Next->Prev = p->Prev;
+        }
+        p = NULL;
+        this->size--;
+        delete p;
     }
     void print() {
         Node *node = Head;
-        cout << "HEAD" << Head->value << endl;
         while(node) {
             cout << node->value << endl;
             node = node->Next;
@@ -59,13 +61,65 @@ struct List {
     }
 };
 
+struct Set {
+    List *collection = new List();
+    Set() {}
+    uint getSize() {
+        return this->collection->size;
+    }
+    void add(uint number) {
+        this->collection->push(number);
+    }
+    void print() {
+        this->collection->print();
+    }
+    
+    bool has(uint number) {
+        Node *p = this->collection->Head;
+        while(p) {
+            if (p->value == number) return true;
+            p = p->Next;
+        }
+        return false;
+    }
+    void remove(uint number) {
+        this->collection->del(number);
+    }
+    Set operator+(const Set second) {
+        Set newSet;
+        Node *p = this->collection->Head;
+        Node *q = second.collection->Head;
+        while(p) {
+            newSet.add(p->value);
+            p = p->Next;
+        }
+        while(q) {
+            newSet.add(q->value);
+            q = q->Next;
+        }
+    return newSet;
+    }
+    Set operator*(const Set &second) {
+        Set newSet;
+        Node *p = second.collection->Head;    
+        while(p) {
+            if (this->has(p->value) ) newSet.add(p->value);
+            p = p->Next;
+        }
+        return newSet;
+    }
+};
+
 int main() {
-    List *s = new List();
-    s->push(12);
-    s->push(13);
-    s->push(14);
-    s->push(15);
-    s->push(16);
-    s->del(12);
-    s->print();
+    Set a;
+    Set b;
+
+    a.add(1);
+    a.add(2);
+    b.add(525);
+    b.add(2);
+    b.add(4215326);
+    b.add(1);
+    a = a*b;
+    a.print();
 }
