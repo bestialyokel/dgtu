d67947452a6ecaa1131d37768f74ca45121cc6fc
bestialyokel <danil.37910@gmail.com>
2019-11-14 22:02:15 +0300

    praktika i bd

--- DB/lab3/10.sql
+++ DB/lab3/10.sql
@@ -1,2 +1,8 @@
-SELECT ZAVOD.* FROM MENU AS ZAVOD
-	WHERE ZAVOD.C_MENU='C';
+/*Напишите запрос, который выводит название марки, средняя цена автомобиля для которой наибольшая. Нулевые не учитывать. */
+SELECT Z.NAME_REC FROM MENU AS Z 
+	WHERE Z.C_MENU || Z.C_REC = (SELECT A.C_PLANT FROM AUTO AS A
+					WHERE A.COST>0
+					GROUP BY A.C_PLANT
+					ORDER BY AVG(A.COST) DESC LIMIT 1);
+
+/*|| concat strings*/
--- DB/lab3/24.sql
+++ DB/lab3/24.sql
@@ -1,1 +1,11 @@
-SELECT * FROM MENU;
+SELECT Addr.AddrCountry,COUNT(Addr.IDAddress) FROM Addresses AS Addr
+INNER JOIN Persons AS PS on (PS.IDPersons=Addr.IDВладельца)
+INNER JOIN PersonT AS PT on (PS.IDPersons=PT.IngPerson)
+INNER JOIN StudentsT as ST on (PT.IngPerson=ST.IDPerson)
+WHERE (PS.Пол='м' AND (ST.Semestr='7' OR ST.Semestr='8') ) OR (PS.Пол='ж' AND ST.Semestr='9')
+GROUP BY Addr.AddrCountry;
+
+
+/*
+Описать работы inner join
+*/
