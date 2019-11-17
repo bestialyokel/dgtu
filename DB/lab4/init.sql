7c8b319998068665d72a52e821886e042a8f47a2
bestialyokel <danil.37910@gmail.com>
2019-11-17 19:11:59 +0300

    buddy bitmap update, db lab4 tables created

---
+++ DB/lab4/init.sql
@@ -0,0 +1,73 @@
+PRAGMA foreign_keys = ON;
+
+CREATE TABLE IF NOT EXISTS Providers (
+	idProvider INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	name TEXT NOT NULL
+	
+);
+
+CREATE TABLE IF NOT EXISTS Tariffs (
+	idTariff INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	payment REAL CHECK(payment >= 0),
+	period INTEGER CHECK (period >= 0)
+);
+
+CREATE TABLE IF NOT EXISTS Services (
+	idService INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	idProvider INTEGER NOT NULL,
+	idTariff INTEGER, 
+	payment REAL CHECK(payment >= 0),
+	name TEXT NOT NULL,
+	description TEXT,
+	FOREIGN KEY(idProvider) REFERENCES Providers(idProvider),
+	FOREIGN KEY(idTariff) REFERENCES Tariffs(idTariff)
+);
+
+CREATE TABLE IF NOT EXISTS Clients (
+	idClient INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	surname TEXT NOT NULL,
+	name TEXT NOT NULL,
+	patronymic TEXT NOT NULL,
+	phoneNumber TEXT NOT NULL UNIQUE
+);
+
+CREATE TABLE IF NOT EXISTS Contracts (
+	idContract INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	idClient INTEGER NOT NULL,
+	idTariff INTEGER, 
+	address TEXT NOT NULL,
+	type TEXT NOT NULL,
+	conclusionDate INTEGER NOT NULL, /*seconds since 1970 ... */
+	FOREIGN KEY(idClient) REFERENCES Clients(idClient),
+	FOREIGN KEY(idTariff) REFERENCES Tariffs(idTariff)
+);
+
+CREATE TABLE IF NOT EXISTS Appeals (
+	idAppeal INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	idContract INTEGER NOT NULL,
+	status TEXT NOT NULL,
+	appealDate INTEGER NOT NULL,
+	description TEXT CHECK (LENGTH(description) > 20),
+	FOREIGN KEY(idContract) REFERENCES Contracts(idContract)
+);
+
+CREATE TABLE IF NOT EXISTS Jobs (
+	idJob INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	idAppeal INTEGER NOT NULL,
+	startTime INTEGER NOT NULL,
+	complexity TEXT NOT NULL,
+	STATUS TEXT NOT NULL,
+	FOREIGN KEY(idAppeal) REFERENCES Appeals(idAppeal)
+);
+
+CREATE TABLE IF NOT EXISTS Workers (
+	idWorker INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+	idProvider INTEGER NOT NULL,
+	idJob INTEGER,
+	surname TEXT NOT NULL,
+	name TEXT NOT NULL,
+	patronymic TEXT NOT NULL,
+	qualification TEXT CHECK (LENGTH(qualification) > 15),
+	FOREIGN KEY(idProvider) REFERENCES Providers(idProvider),
+	FOREIGN KEY(idJob) REFERENCES Jobs(idJob)
+);
--- OS/buddy_bitmap.cpp
+++ OS/buddy_bitmap.cpp
@@ -18,39 +18,50 @@ uint32_t clp2(uint32_t x) {
 uint memory[256];
 uint BLOCK_SIZE = 8;
 uint bitmap = 0; // 32 blocks
-
-uint TYPES_AMOUNT = 6; // stepeni
-
+uint TYPES_AMOUNT = 6; // stepenei
 uint *BITMAPS_LIST = new uint[6];
 
-int allocate(uint size, uint reqLevel, uint currLevel) {
-    if (currLevel > TYPES_AMOUNT - 1) return -1;
-    //for (uint i = 0; i < (1 << curr))
+int allocate(size_t);
+bool isFree(uint, size_t);
+void deallocate(uint);
+int allocate_from_spread(uint);
+void set_bitmap(uint, size_t, bool);
+
+void fill_bitmap(uint start, size_t amount) {
+    while (amount > 0) bitmap |= (1 << start + --amount);
+}
+void free_bitmap(uint start, size_t amount) {
+    while (amount > 0) bitmap &= ~(1 << start + --amount);
 }
 
-int allocate(uint size) {
-    size = clp2(size);
-    size = size < BLOCK_SIZE ? 8 : size;
+int allocate(size_t size) {
+    size = size < BLOCK_SIZE ? BLOCK_SIZE : clp2(size);
     uint level = 0;
     while (BLOCK_SIZE << level < size) level++;
     level = TYPES_AMOUNT - level - 1;
-    for (uint i = 0; i < (1<<level); i++) {
-        if (isFree())
+    uint bmap = BITMAPS_LIST[level];
+    for(uint i = 0; i < (1 << level); i++) {
+        if (bmap & (1<<i) && isFree(i*(1 << level), (1 << level))) {
+            for(uint j = 0; j < (1 << level); j++) {
+                bitmap |= i*
+            }
+            return i*(1 << level);
+        }
     }
+    return allocate_from_spread(level,)
     
 }
 
 bool isFree(uint start, uint amount) {
     while (amount > 0) {
-        if (bitmap & (1 << start + amount--)) return false;
+        if (bitmap & (1 << start + --amount)) return false;
     }
     return true;
 }
 
 
 int main() {
-    BITMAPS_LIST[5] = 1;
-    allocate(2);
-
+    BITMAPS_LIST[0] = 1;
+    p(allocate(8));
     return 0;
 }
