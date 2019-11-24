c673bbfdd2cdae485b3fb9bfb56ef4aa6e651b98
bestialyokel <danil.37910@gmail.com>
2019-11-24 22:15:40 +0300

    oop/lab4 array edit

--- OOP/lab4/array.cpp
+++ OOP/lab4/array.cpp
@@ -5,17 +5,32 @@ using namespace std;
 
 class Array {
     private:
-        int *arr;
+        int *arr = NULL;
     public:
         size_t length = 0;
         Array(){}
         void remove(size_t index) {
+            if (this->length == 0) return;
+            if (this->length == 1) {
+                this->arr = NULL;
+                this->length = 0;
+                return;
+            }
             for (int i = index; i < this->length; i++) this->arr[i] = this->arr[i+1];
             this->length--;
             this->arr = (int *)realloc(this->arr, this->length * sizeof(int));
         }
         void push(int value) {
-            cout << (this->arr == NULL) << endl;
+            if (this->arr == NULL) {
+                this->arr = new int[1];
+                this->arr[0] = value;
+                this->length++;
+            }
+            else {
+                this->length++;
+                this->arr = (int *)realloc(this->arr, this->length * sizeof(int));
+                this->arr[this->length - 1] = value;
+            }
         }
         int& operator[](size_t index) {
             if (index < this->length) return this->arr[index];
@@ -30,8 +45,11 @@ class Array {
 int main() {
     Array a;
     a.push(12);
-    for (int i = 0; i < a.length; i++) {
-        cout << a[i] << endl;
-    }
+    a.push(2);
+    a.push(2412);
+    a.push(25);
+    a.remove(2);
+    cout << a[0];
+    for (int i = 0; i < a.length; i++) cout << a[i] << endl;
     return 0;
 }
