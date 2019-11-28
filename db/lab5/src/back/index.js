5f9e562638044259fbb2962468a02502950a33cb
bestialyokel <danil.37910@gmail.com>
2019-11-28 16:21:35 +0300

    db lab5 restart

--- db/lab5/src/back/index.js
+++ db/lab5/src/back/index.js
@@ -4,23 +4,11 @@ const PORT = 8080;
 
 
 const db = require('./db/database.js');
-const sequelize= require('sequelize');
 const express = require('express');
 const rules = require('./rules.js');
 
-//ебаный колхоз
-
-const Appeal = require('./models/appeal.js')(db, sequelize);
-const Client = require('./models/client.js')(db, sequelize);
-const Contract = require('./models/contract.js')(db, sequelize);
-const Job = require('./models/job.js')(db, sequelize);
-const Service = require('./models/service.js')(db, sequelize);
-const Tariff = require('./models/tariff.js')(db, sequelize);
-const Worker = require('./models/worker.js')(db, sequelize);
-const User = require('./models/user.js')(db, sequelize);
-const Provider = require('./models/provider.js')(db, sequelize);
 
 
 const app = express();
 
-Service.findAll().then(x => console.log(x));
+
