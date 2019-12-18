be1ef6b536c75bf549b4d8ef5ebe5035529dc5a6
bestialyokel <bestialyokel@localhost.localdomain>
2019-12-19 01:42:43 +0300

    c# 4+5

--- c#/lab4.cs
+++ c#/lab4.cs
@@ -1,66 +1,172 @@
 using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-public interface IEelectric {
-    int voltage {get; set;}
-    int MAX_POWER {get; set;}
-} 
-
-public interface IEelectricTransmitter : IEelectric {
-    IEelectric[] plugged {get; set;}
+//интерфейс источника тока 
+public interface IElectricSource {
+    uint Voltage();
+    uint MaxPower();
 }
 
-public interface IElectricSource : IEelectric, IEelectricTransmitter {
-
+//интерфейс электрического прибора 
+public interface IElectricAppliance {
+    uint Voltage();
+    uint MaxPower();
 }
 
-public interface IElectricAppliance : IEelectric {
-    string name {get; set;}
-    int reqPower {get; set}
-
-}
-
-public interface IElectricWire : IEelectric, IEelectricTransmitter {
-
+//интерфейс электрического шнура  
+public interface IElectricWire {
+    uint Voltage();
+    uint MaxPower();
 }
 
 
-public class SolarBattery : IElectricSource {
 
+class PowerSocket : IElectricSource {
+    private uint voltage;
+    private uint maxpower;
+    public List<string> appli = new List<string>();
+
+    public PowerSocket(uint x = 220, uint y = 300) {
+        this.voltage = x;
+        this.maxpower = y;
+    }
+
+    public uint Voltage() => this.voltage;
+
+    public uint MaxPower() => this.maxpower;
+
+    public void show() {
+        Console.WriteLine(" Список подключенных прибороб : ");
+
+        if (appli.Count == 0) 
+            Console.WriteLine(" Подключений нет ");
+        else 
+            for (int i = 0; i < appli.Count; i++) 
+                Console.WriteLine(" Подключен : {0}  ", appli[i]);
+
+    }
 }
 
-public class DieselGenerator : IElectricSource {
+
+class Wire : IElectricWire {
+    private uint voltage;
+    private uint maxpower;
+
+    public PowerSocket obj;
+
+    public Wire(PowerSocket ps, uint x = 220, uint y = 300) {
+        this.obj = ps;
+        this.voltage = x;
+        this.maxpower = y;
+    }
+
+    public uint Voltage() => this.voltage;
+    public uint MaxPower() => this.maxpower;
+
+    public delegate void off();
+    public delegate void on();
+
+    public event off TO_OFF;
+    public event on TO_ON;
+
+    public void ON() => TO_ON();
+
+    public void OFF() => TO_OFF();
+
+    public void show() {
+        Console.WriteLine(" Список подключенных прибороб : ");
+
+        if (obj.appli.Count == 0) 
+            Console.WriteLine(" Подключений нет ");
+        else 
+            for (int i = 0; i < obj.appli.Count; i++) 
+                Console.WriteLine("Подключен : {0}  ", obj.appli[i]);
     
+        Console.WriteLine();
+    }
+
 }
 
-public class NuclearPowerPlant : IElectricSource {
-    
+class Netbook : IElectricAppliance {
+    private uint voltage;
+    private uint maxpower;
+    private string name;
+    private PowerSocket obj;
+
+    public Netbook(PowerSocket obj, uint x = 220, uint y = 300, string nam = " NetBook ") {
+        this.voltage = x;
+        this.maxpower = y;
+        this.name = nam;
+        this.obj = obj;
+    }
+    public uint Voltage() => this.voltage;
+
+    public uint MaxPower() => this.maxpower;
+
+    public void on() {
+        this.obj.appli.Add(this.name);
+    }
+
+    public void off() {
+        this.obj.appli.Remove(this.name);
+    }
+
+    public void show() {
+        Console.WriteLine(this.name);
+    }
+
 }
 
-public class Kettle : IElectricAppliance {
-    
-}
+class Kettle : IElectricAppliance {
+    private uint voltage;
+    private uint maxpower;
+    private string name;
+    private PowerSocket obj;
+    public Kettle(PowerSocket obj, uint x = 220, uint y = 300, string nam = " Kettle ") {
+        this.voltage = x;
+        this.maxpower = y;
+        this.name = nam;
+        this.obj = obj;
+    }
+    public uint Voltage() => this.voltage;
 
-public class Lathe : IElectricAppliance {
-    
-}
+    public uint MaxPower() => this.maxpower;
 
-public class Refrigerator : IElectricAppliance {
-    
-}
+    public void on() {
+        this.obj.appli.Add(this.name);
+    }
 
-public class HighLine : IElectricWire {
-    
-}
+    public void off() {
+        this.obj.appli.Remove(this.name);
+    }
 
-public class StepDownTransformer : IElectricAppliance, IElectricSource {
-    
+    public void show() {
+        Console.WriteLine(this.name);
+    }
 }
-
-public class ElectricPowerStrip : IElectricWire {
-    
-}
-
 class lab4 {
-    static void Main() {
+    static void Main(string[] args) {
+        PowerSocket p = new PowerSocket();
+        Wire a = new Wire(p);
+        Netbook nb = new Netbook(p);
+        Kettle kt = new Kettle(p);
+
+        a.TO_ON += nb.on;
+        a.TO_OFF += nb.off;
+
+        a.TO_ON += kt.on;
+        a.TO_OFF += kt.off;
+
+        a.ON();
+
+        a.show();
+
+        a.OFF();
+        a.show();
+
+        Console.ReadLine();
     }
 }
--- c#/lab5.cs
+++ c#/lab5.cs
@@ -4,10 +4,67 @@ using System.Collections.Generic;
 
 
 
-
-
 class lab5 {
-    static void Main() {
-        Set<uint> s = new Set<uint>();
+
+
+
+    public static bool isPrime(uint n) {// Дорого ?
+        uint d = 2;
+        while (n % d != 0) 
+            d += 1;
+        return d == n;
     }
+
+    public class Primes : IEnumerable{
+        private uint amount;
+
+        public Primes(uint amount) {
+            this.amount = amount;
+        }
+
+        public IEnumerator GetEnumerator() {
+            uint n = this.amount;
+            if (n-- > 0) yield return 2;
+            if (n-- > 1) yield return 3;
+            uint num = 5;
+            while(n > 0) {
+                if (isPrime(num)) {
+                    n--;
+                    yield return num;
+                }
+                num += 2;
+            }
+        }
+        IEnumerator IEnumerable.GetEnumerator() {
+            return GetEnumerator();
+        }
+    }
+
+
+    public static IEnumerable NPrimes(uint n) {
+        if (n-- > 0) yield return 2;
+        if (n-- > 1) yield return 3;
+        
+        uint num = 5;
+        while(n > 0) {
+            if (isPrime(num)) {
+                n--;
+                yield return num;
+            }
+            num += 2;
+        }
+    }
+    
+
+
+
+
+
+    public static void Main() {
+        IEnumerable a = NPrimes(10);
+        Primes b = new Primes(10);
+        foreach (var c in b) Console.WriteLine(c);
+    }
+
+
 }
