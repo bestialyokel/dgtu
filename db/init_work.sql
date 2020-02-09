9e3328c690674c4b2f0a42527d7fd82bcaee62ac
bestialyokel <bestialyokel@github.com>
2020-02-09 21:48:02 +0300

    rollbacks upd

--- db/init_work.sql
+++ db/init_work.sql
@@ -1,8 +1,10 @@
 /*
 *
-*  ROLLBACKS* MADE WITH GUIDE : http://www.varlena.com/GeneralBits/122.php
+*  Temp* MADE WITH GUIDE : http://www.varlena.com/GeneralBits/122.php
 *
 */
+
+-- КОПИПАСТ MONSTER14288
 
 /*
 *
@@ -433,11 +435,68 @@ CREATE TRIGGER Workers_trigger AFTER INSERT OR UPDATE OR DELETE ON Workers FOR E
 
 \i fill.sql
 
-CREATE OR REPLACE FUNCTION
-
 /*TG_TABLE_SCHEMA
 *
 *
 * TODO: сдеалать роллбеки для каждой таблицы.g
-*
+* хз работает ли
 */
+
+CREATE OR REPLACE PROCEDURE ROLLBACK_SERVICE(id integer, to_date timestamp) AS $$
+    DECLARE
+        _service_rec RECORD;
+        updated_id integer;
+    BEGIN
+        SELECT INTO _service_rec * 
+            FROM Services_tmp as STmp
+            WHERE STmp.id_service = id AND STmp.create_date <= to_date
+            ORDER BY create_date DESC
+            LIMIT 1;
+        -- not created yet?
+        IF _service_rec IS NULL THEN 
+            DELETE FROM Services WHERE id_service = id;
+            RETURN;
+        END IF;
+        -- try update
+        UPDATE Services SET 
+            name = _service_rec.name, 
+            description = _service_rec.description
+        WHERE id_service = _service_rec.id_service RETURNING id_service;
+        -- if wasnt updated -> insert???
+        IF NOT FOUND THEN
+            INSERT INTO Services(id_service, name, description) VALUES (_service_rec.id_service, _service_rec.name, _service_rec.description);
+        END IF;
+    END;
+$$ LANGUAGE plpgsql;
+
+
+
+CREATE OR REPLACE PROCEDURE ROLLBACK_TARIFF(id integer, to_date timestamp) AS $$
+    DECLARE
+        _tariff_rec RECORD;
+    BEGIN
+        SELECT INTO _tariff_rec * 
+            FROM Tariffs_tmp as TTmp
+            WHERE TTmp.id_tariff = id AND TTmp.create_date <= to_date
+            ORDER BY create_date DESC
+            LIMIT 1;
+        -- not created yet?
+        IF _tariff_rec IS NULL THEN 
+            DELETE FROM Tariffs WHERE id_tariff = id;
+            RETURN;
+        END IF;
+        -- update & save updated_id
+        UPDATE Tariffs SET 
+            name = _tariff_rec.name, 
+            payment = _tariff_rec.period,
+            period = _tariff_rec.period
+        WHERE id_tariff = _tariff_rec.id_tariff RETURNING id_tariff;
+
+        -- if wasnt updated???
+        IF NOT FOUND THEN
+            INSERT INTO Tariffs(id_tariff, payment, patronymic) VALUES (_tariff_rec.id_tariff, _tariff_rec.payment, _tariff_rec.patronymic);
+        END IF;
+    END;
+$$ LANGUAGE plpgsql;
+
+-- подумать над откатами еще(каскадно?) 
