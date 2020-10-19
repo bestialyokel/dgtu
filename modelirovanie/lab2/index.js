e4948f9069aa301c47521ea432ec9432e2be00e1
bestialyokel <bestialyokel@github.com>
2020-10-19 21:48:43 +0300

    hz

--- modelirovanie/lab2/index.js
+++ modelirovanie/lab2/index.js
@@ -7,15 +7,14 @@ const K_FILE = 'kabara';
 
 //L = 150
 const N = 150;
-const h = 0.001;
+const h = 1;
 
 //время
-const T = 3;
+const T = 1.5;
 const t = 0.05;
 let t0 = 0;
 
 const a = 0.999;
-
 
 const y = a * t / h;
 
@@ -27,25 +26,29 @@ let PREV = Array(N).fill(0);
 let CUR = PREV.slice();
 let NEXT = CUR.slice();
 
-
 const output = (filename) => {
     fs.truncateSync(filename);
     for (let i = 0; i < N; i++) {
-        const str = `${CUR[i].toFixed(30)} ${i*h}${os.EOL}`;
+        const str = `${CUR[i].toFixed(30)}${os.EOL}`;
+        fs.appendFileSync(filename, str);
+    }
+
+    for (let i = 0; i < N; i++) {
+        const str = `${i*h}${os.EOL}`;
         fs.appendFileSync(filename, str);
     }
     
 }
 
 
-PREV[0] = 1;
 CUR[0] = 1;
+PREV[0] = 1;
+
 
 //Явная схема(Уголок)
-//устойчива только при a >= 0 
+//устойчива только при a >= 0
 // и y <=1;
 do {
-
     for (let i = 1; i < N; i++) {
         NEXT[i] = CUR[i] - y * (CUR[i] - CUR[i-1]); 
     }
@@ -64,21 +67,19 @@ output(U_FILE);
 //-------------------------------------------------------------------------
 
 
+
 PREV = Array(N).fill(0);
 CUR = PREV.slice();
 NEXT = CUR.slice();
 
 
-
-
-PREV[0] = 1;
 CUR[0] = 1;
+PREV[0] = 1;
 
-//Явная схема(Уголок)
+//Центральная схема
 //устойчива только при a >= 0 
 // и y <=1;
 do {
-
     for (let i = 1; i < N - 1; i++) {
         NEXT[i] = CUR[i] - y * (CUR[i+1] - CUR[i-1])/2; 
     }
@@ -101,10 +102,11 @@ CUR = PREV.slice();
 NEXT = CUR.slice();
 
 
-PREV[0] = 1;
 CUR[0] = 1;
+PREV[0] = 1;
 
-//Явная схема(Уголок)
+
+//Кабаре
 //устойчива только при a >= 0 
 // и y <=1;
 do {
@@ -123,3 +125,4 @@ do {
 
 
 output(K_FILE);
+
